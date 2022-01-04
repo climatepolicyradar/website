8b65494859d99e3bd6116f31f8b08bd9c2626fe2
Paula Hightower <eurolife@gmail.com>
2022-01-04 11:28:00 +0000

    cleanup

--- src/lib/Components/ValuePanel.svelte
+++ src/lib/Components/ValuePanel.svelte
@@ -1,72 +1,42 @@
 <script>
-  import { onMount } from 'svelte';
-  import { fade } from "svelte/transition";
+  import { fade } from 'svelte/transition';
   import Icon from '$lib/Components/Icon.svelte';
   export let icon;
   export let title;
   export let description1;
   export let description2;
-
-  onMount(() => {
-
-  })
 </script>
 
-  <div class="c-panel" transition:fade>
-    <div class="c-panel__col c-panel__col-title left">
-      <Icon name={icon} />
-      <h4 class="c-panel__title">{title}</h4>
-    </div>
-    <div class="c-panel__col c-panel__col-text right">
-      <p>{description1}</p>
-      <p>{description2}</p>
-    </div>
+<div class="c-panel" transition:fade>
+  <div class="c-panel__col c-panel__col-title left">
+    <Icon name={icon} />
+    <h4 class="c-panel__title">{title}</h4>
   </div>
+  <div class="c-panel__col c-panel__col-text right">
+    <p>{description1}</p>
+    <p>{description2}</p>
+  </div>
+</div>
 
 <style>
-  
-  
+  .c-panel {
+    display: flex;
+    flex-direction: column;
+  }
   .c-panel:not(:first-child) {
     margin-top: 32px;
   }
   @media (min-width: 768px) {
-    .c-panel {
-      display: flex;
-    }
-    
-  }
-  @media (min-width: 1024px) {
-    .c-panel {
-      max-width: 700px;
-      
-    }
-    .c-panel:nth-child(odd) {
-      margin-right: auto;
-    }
-    .c-panel:nth-child(even) {
-      margin-left: auto;
+    .c-panel:not(:first-child) {
+      margin-top: 0;
     }
   }
-  .c-panel__title {
-    font-size: 16px;
-    padding-left: 8px;
-  }
-  @media (min-width: 768px) {
-    .c-panel__title {
-      padding-left: 0;
-      text-align: center;
-      margin-top: 16px;
-    }
-  }
-  .c-panel__col {
 
-  }
   .c-panel__col-title {
-    
     background: var(--color-indigo);
     color: white;
     display: flex;
-    
+
     justify-content: flex-start;
     align-items: center;
     padding: 16px;
@@ -75,29 +45,20 @@
   }
   @media (min-width: 768px) {
     .c-panel__col-title {
-      justify-content: center;
-      flex-direction: column;
-      width: 25%;
-      text-align: center;
-      border-top-right-radius: 0;
-      border-top-left-radius: 30px;
-      border-bottom-left-radius: 30px;
-
+      padding: 24px;
     }
-    /* .c-panel__col-title.left {
-      border-top-right-radius: 0;
-      border-top-left-radius: 30px;
-      border-bottom-left-radius: 30px;
-      order: 1;
-    }
-    .c-panel__col-title.right {
-      border-top-left-radius: 0;
-      border-top-right-radius: 30px;
-      border-bottom-right-radius: 30px;
-      order: 2;
-    } */
   }
-  
+
+  .c-panel__title {
+    font-size: 16px;
+    padding-left: 8px;
+  }
+  @media (min-width: 768px) {
+    .c-panel__title {
+      font-size: 24px;
+    }
+  }
+
   .c-panel__col-text {
     background: var(--color-indigo-200);
     padding: 16px;
@@ -108,23 +69,9 @@
   }
   @media (min-width: 768px) {
     .c-panel__col-text {
-      width: 75%;
+      padding: 24px;
       font-size: 16px;
-      border-bottom-left-radius: 0;
-      border-top-right-radius: 30px;
-      border-bottom-right-radius: 30px;
+      flex-grow: 1;
     }
-    /* .c-panel__col-text.left {
-      border-bottom-right-radius: 0;
-      border-top-left-radius: 30px;
-      border-bottom-left-radius: 30px;
-      order: 1;
-    }
-    .c-panel__col-text.right {
-      border-bottom-left-radius: 0;
-      border-top-right-radius: 30px;
-      border-bottom-right-radius: 30px;
-      order: 2;
-    } */
   }
 </style>
