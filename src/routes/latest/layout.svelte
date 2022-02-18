b794be537152762eccee25f83df3bda10028daae
Paula Hightower <eurolife@gmail.com>
2022-02-18 14:22:31 +0000

    add more social meta tags

--- src/routes/latest/layout.svelte
+++ src/routes/latest/layout.svelte
@@ -46,11 +46,21 @@
 <svelte:head>
   <title>{title} | Climate Policy Radar</title>
   <meta name="description" content={excerpt} />
+  <meta property="og:type" content="article" />
   <meta name="title" property="og:title" content={title} />
   <meta name="description" property="og:description" content={excerpt} />
   <meta
     name="image"
     property="og:image"
+    content={`https://climatepolicyradar.org${thumb}`}
+  />
+  <meta name="twitter:card" content="summary" />
+  <meta name="twitter:site" content="@climatepolradar" />
+  <meta name="twitter:title" content={title} />
+  <meta name="twitter:description" content={excerpt} />
+  <meta name="twitter:creator" content="@climatepolradar" />
+  <meta
+    name="twitter:image"
     content={`https://climatepolicyradar.org${thumb}`}
   />
 </svelte:head>
