fb6565a60d8fbb3e507cae07bc158106da7dd263
Paula Hightower <eurolife@gmail.com>
2022-03-14 12:59:04 +0000

    Google analytics

---
+++ src/lib/Components/GoogleAnalytics.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { page } from '$app/stores';
+  $: {
+    if (typeof gtag !== 'undefined') {
+      gtag('config', 'GTM-TBB5CMS', {
+        page_path: $page.path,
+      });
+    }
+  }
+</script>
