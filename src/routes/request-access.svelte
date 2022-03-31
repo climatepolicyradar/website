7ae9c42ced3c1326044cb9cf74f81de94c145ca5
Paula Hightower <eurolife@gmail.com>
2022-03-31 16:38:18 +0100

    change launch date

--- src/routes/request-access.svelte
+++ src/routes/request-access.svelte
@@ -30,8 +30,8 @@
   </svelte:fragment>
   <p class="u-type-body-lg">
     We are working towards our first limited product release to a small group of
-    users in March 2022. If you would like to have early access, please leave
-    your details below, and we will be in touch.
+    users in May 2022. If you would like to have early access, please leave your
+    details below, and we will be in touch.
   </p>
 </Banner>
 
