688fe5546a1ef4b210dc5352c37816f90fe6a19b
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2022-07-22 14:01:38 +0100

    Add video SEO copy

--- src/lib/Blocks/Video.svelte
+++ src/lib/Blocks/Video.svelte
@@ -1,11 +1,15 @@
 <script>
   import Wrapper from '$lib/Components/Wrapper.svelte';
+  import SingleColumn from '$lib/Blocks/SingleColumn.svelte';
 </script>
 
 <Wrapper>
   <div class="video-container">
     <iframe src="https://www.youtube-nocookie.com/embed/zrpA5n9Z93Y?rel=0&playlist=zrpA5n9Z93Y&loop=1&modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </div>
+  <p class="u-type-body-xxxl u-text-center">
+    How to use Climate Policy Radar's search tool to search thousands of climate policies, laws and strategies
+  </p>
 </Wrapper>
 
 <style>
