256308828c1a9a834396822e16c016fa6407eb00
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2022-11-15 12:42:44 +0000

    Fix footer

--- src/lib/stores/dataScience.js
+++ src/lib/stores/dataScience.js
@@ -1,23 +1,30 @@
-import { readable, writable } from 'svelte/store';
+import { readable, writable } from 'svelte/store'
 
-const activeTabIndex = writable(0);
+const activeTabIndex = writable(0)
 
 const dataScience = readable([
   {
     title: 'Extracting text: document parsing',
     description1:
-      'We are building and training machine learning models that can automatically read and extract text from PDFs and websites, enabling us to structure and share information from thousands of law and policy documents. This will mean we can massively ramp up the speed of data collection as we find new data sources. <a href="https://github.com/climatepolicyradar" target="_blank">Access our GitHub page for more information</a>.',
+      'We are building and training machine learning models that can automatically read and extract text from PDFs and websites, enabling us to structure and share information from thousands of law and policy documents.',
+    description2:
+      'This will mean we can massively ramp up the speed of data collection as we find new data sources. <a href="https://github.com/climatepolicyradar" target="_blank">Access our GitHub page for more information</a>.',
     icon: 'text',
   },
   {
     title: 'Natural language search',
-    description1: 'Our application of machine learning is tackling blind spots in policy research by making it quicker, easier and more intuitive to search climate policies and laws. Our tool identifies similar and related terms to search queries, so there is no need to rely on typing specific keywords into the search bar to find relevant information. <a href="https://climatepolicyradar.org/latest/building-natural-language-search-for-climate-change-laws-and-policies" target="_blank">Find out how our natural language search works</a>.',
+    description1:
+      'Our application of machine learning is tackling blind spots in policy research by making it quicker, easier and more intuitive to search climate policies and laws.',
+    description2:
+      'Our tool identifies similar and related terms to search queries, so there is no need to rely on typing specific keywords into the search bar to find relevant information. <a href="https://climatepolicyradar.org/latest/building-natural-language-search-for-climate-change-laws-and-policies" target="_blank">Find out how our natural language search works</a>.',
     icon: 'semanticSearch',
   },
   {
     title: 'Text classification and information retrieval',
     description1:
-      'We are training machine learning models to identify and link useful concepts across the texts of thousands of documents. This will allow understanding of, for example, how governments address different extreme weather events, or regulate various environmental technologies. Using existing taxonomies and new bespoke ones, we will help identify concepts quickly, pinpoint trends and highlight gaps.',
+      'We are training machine learning models to identify and link useful concepts across the texts of thousands of documents. This will allow understanding of, for example, how governments address different extreme weather events, or regulate various environmental technologies. ',
+    description2:
+      'Using existing taxonomies and new bespoke ones, we will help identify concepts quickly, pinpoint trends and highlight gaps.',
     icon: 'textMagnifier',
   },
   {
@@ -31,15 +38,19 @@ const dataScience = readable([
   {
     title: 'Open knowledge graph',
     description1:
-      'We are working towards an open knowledge graph that joins the dots between points in our growing structured dataset, revealing gaps, risks and opportunities. This will open up the ability to explore our data in new ways, and connect it to other datasets and APIs to start to measure the impacts of policy decisions.',
+      'We are working towards an open knowledge graph that joins the dots between points in our growing structured dataset, revealing gaps, risks and opportunities.',
+    description2:
+      'This will open up the ability to explore our data in new ways, and connect it to other datasets and APIs to start to measure the impacts of policy decisions.',
     icon: 'knowledge',
   },
   {
     title: 'Analytical tools',
     description1:
-      'We will develop tools to allow stakeholders to generate their own insights, including linking our data with ‘real-world’ external data like carbon emissions. This will make it easier to link actions to outcomes, and facilitate modelling of policy effectiveness and risk analyses.',
+      'We will develop tools to allow stakeholders to generate their own insights, including linking our data with ‘real-world’ external data like carbon emissions.',
+    description2:
+      'This will make it easier to link actions to outcomes, and facilitate modelling of policy effectiveness and risk analyses.',
     icon: 'graph2',
   },
-]);
+])
 
-export { dataScience, activeTabIndex };
+export { dataScience, activeTabIndex }
