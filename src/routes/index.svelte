5ef66c23b9cb2d7e41eefcd632e488bf84fc7921
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2022-12-22 09:38:10 +0000

    H1 container to full width

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -46,7 +46,7 @@
 
 <MetaTags {title} {excerpt} {image} {path} />
 
-<Banner size="lg" theme="dark">
+<Banner size="lg" theme="dark" contentType="full-width">
   <h1 slot="heading">Building the evidence base for evidence-based decision-making</h1>
   <p slot="subheading">
     Giving policymakers, researchers and civil society tools for better climate policy design and accountability
